30d5d082a414abe6fe1f546db65ca9772f997104
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-04 23:48:36 +0200

    Implement Model for Trade

--- x/orderbook/model.go
+++ x/orderbook/model.go
@@ -93,28 +93,6 @@ func (o *OrderBook) Validate() error {
 	return nil
 }
 
-// message Order {
-// 	weave.Metadata metadata = 1;
-// 	bytes id = 2 [(gogoproto.customname) = "ID"];
-// 	// Address of trader that created order (ad gets paid from it)
-// 	bytes trader = 3 [(gogoproto.casttype) = "github.com/iov-one/weave.Address"];
-// 	bytes order_book_id = 4 [(gogoproto.customname) = "OrderBookID"];
-// 	Side side = 5;
-// 	OrderState order_state = 6;
-// 	// The amount maker want to sell/buy
-// 	coin.Coin original_offer = 7;
-// 	// Remaning offer. (This part enables partially filled orders)
-// 	coin.Coin remaining_offer = 8;
-// 	// Price for each unit (in tickers of the opposite currency from our side)
-// 	Amount price = 9;
-// 	// Executed trades (referenced ids)
-// 	repeated bytes trade_ids = 10;
-// 	// created_at defines create time of an order
-// 	int64 created_at = 11 [(gogoproto.casttype) = "github.com/iov-one/weave.UnixTime"];
-// 	// updated_at defines update time of an order
-// 	int64 updated_at = 12 [(gogoproto.casttype) = "github.com/iov-one/weave.UnixTime"];
-//   }
-
 var _ morm.Model = (*Order)(nil)
 
 // SetID is a minimal implementation, useful when the ID is a separate protobuf field
@@ -163,6 +141,7 @@ func (o *Order) Validate() error {
 	if err := o.RemainingOffer.Validate(); err != nil {
 		return errors.Wrap(err, "remaining offer")
 	}
+
 	// TODO: valid price
 	// TODO: valid trade ids (also rethink how we handle this? just use index and not in model?)
 
@@ -177,6 +156,64 @@ func (o *Order) Validate() error {
 	}
 	if o.CreatedAt == 0 {
 		return errors.Wrap(errors.ErrEmpty, "missing created at")
+	}
+	return nil
+}
+
+var _ morm.Model = (*Trade)(nil)
+
+// SetID is a minimal implementation, useful when the ID is a separate protobuf field
+func (t *Trade) SetID(id []byte) error {
+	t.ID = id
+	return nil
+}
+
+// Copy produces a new copy to fulfill the Model interface
+func (t *Trade) Copy() orm.CloneableData {
+	return &Trade{
+		Metadata:    t.Metadata.Copy(),
+		ID:          copyBytes(t.ID),
+		OrderBookID: copyBytes(t.OrderBookID),
+		OrderID:     copyBytes(t.OrderID),
+		Taker:       copyBytes(t.Taker),
+		Maker:       copyBytes(t.Maker),
+		MakerPaid:   t.MakerPaid.Clone(),
+		TakerPaid:   t.TakerPaid.Clone(),
+		ExecutedAt:  t.ExecutedAt,
+	}
+}
+
+// Validate is always succesful
+func (o *Trade) Validate() error {
+	if err := o.Metadata.Validate(); err != nil {
+		return err
+	}
+	if err := isGenID(o.ID, true); err != nil {
+		return err
+	}
+	if err := isGenID(o.OrderBookID, false); err != nil {
+		return errors.Wrap(err, "order book id")
+	}
+	if err := isGenID(o.OrderID, false); err != nil {
+		return errors.Wrap(err, "order  id")
+	}
+	if err := o.Taker.Validate(); err != nil {
+		return errors.Wrap(err, "taker")
+	}
+	if err := o.Maker.Validate(); err != nil {
+		return errors.Wrap(err, "maker")
+	}
+	if err := o.MakerPaid.Validate(); err != nil {
+		return errors.Wrap(err, "maker paid")
+	}
+	if err := o.TakerPaid.Validate(); err != nil {
+		return errors.Wrap(err, "taker paid")
+	}
+	if err := o.ExecutedAt.Validate(); err != nil {
+		return errors.Wrap(err, "executed at")
+	}
+	if o.ExecutedAt == 0 {
+		return errors.Wrap(errors.ErrEmpty, "missing executed at")
 	}
 	return nil
 }
