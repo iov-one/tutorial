4c0d6bfe6f6c70e6eb242c9b4b307ba3d582ef16
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-06 20:51:48 +0200

    Add OrderBook with open-order index to search matches

--- x/orderbook/bucket.go
+++ x/orderbook/bucket.go
@@ -41,3 +41,61 @@ func marketIDindexer(obj orm.Object) ([]byte, error) {
 	}
 	return ob.MarketID, nil
 }
+
+type OrderBucket struct {
+	morm.ModelBucket
+}
+
+func NewOrderBucket() *OrderBucket {
+	b := morm.NewModelBucket("order", &Order{},
+		morm.WithIndex("open", openOrderIDindexer, false),
+	)
+	return &OrderBucket{
+		ModelBucket: b,
+	}
+}
+
+// in SQL parlance, this is a compound index:
+//
+//   (OrderBookID, Side, Price) WHERE order.OrderState = Open
+//
+// The purpose is to enable range proofs over price for matching order...
+// eg. (orderbook=7, side=ask) and then and Iterate over prices Ascending
+// (TODO: add a proper Iterator/First method to ModelBucket - key and index)
+func openOrderIDindexer(obj orm.Object) ([]byte, error) {
+	if obj == nil || obj.Value() == nil {
+		return nil, nil
+	}
+	order, ok := obj.Value().(*Order)
+	if !ok {
+		return nil, errors.Wrapf(errors.ErrState, "expected order, got %T", obj.Value())
+	}
+	return BuildOpenOrderIndex(order)
+}
+
+// Goal is compound index like:
+//
+//   (OrderBookID, Side, Price) WHERE order.OrderState = Open
+//
+// Stored as - 8 bytes bigendian OrderBookID, 1 byte Side, 8 byte bigendian Price.Whole, 8 byte bigendian Price.Fractional
+// We use Price.Lexographic() to produce a lexographic ordering, such than
+//
+//   A.Lexographic() < B.Lexographic == A < B
+//
+// This is a very nice trick to get clean range queries over sensible value ranges in a key-value store
+func BuildOpenOrderIndex(order *Order) ([]byte, error) {
+	// we don't index if state isn't open
+	if order.OrderState != OrderState_Open {
+		return nil, nil
+	}
+
+	res := make([]byte, 9, 9+16)
+	copy(res, order.OrderBookID)
+	res[8] = byte(order.Side)
+	lex, err := order.Price.Lexographic()
+	if err != nil {
+		return nil, errors.Wrap(err, "building order index")
+	}
+	copy(res[9:], lex)
+	return res, nil
+}
