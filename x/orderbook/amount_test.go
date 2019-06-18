2b69f4e19697cd2a6a0948aff6d0e14a01b2988b
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-18 15:14:27 +0200

    Add basic amount tests

--- x/orderbook/amount_test.go
+++ x/orderbook/amount_test.go
@@ -8,16 +8,16 @@ import (
 )
 
 func TestAmountLexographicEncoding(t *testing.T) {
-	cases := map[string]struct{
-		a Amount
+	cases := map[string]struct {
+		a      Amount
 		expect []byte
 	}{
 		"one byte encoding": {
-			a: Amount{Whole: 123, Fractional: 66},
+			a:      Amount{Whole: 123, Fractional: 66},
 			expect: []byte{0, 0, 0, 0, 0, 0, 0, 123, 0, 0, 0, 0, 0, 0, 0, 66},
 		},
 		"multi byte encoding": {
-			a: Amount{Whole: 0x7a4501, Fractional: 12345},
+			a:      Amount{Whole: 0x7a4501, Fractional: 12345},
 			expect: []byte{0, 0, 0, 0, 0, 0x7a, 0x45, 0x01, 0, 0, 0, 0, 0, 0, 0x30, 0x39},
 		},
 	}
@@ -32,19 +32,19 @@ func TestAmountLexographicEncoding(t *testing.T) {
 }
 
 func TestAmountLexographic(t *testing.T) {
-	cases := map[string]struct{
-		a Amount
-		b Amount
+	cases := map[string]struct {
+		a            Amount
+		b            Amount
 		expectBigger bool
 	}{
 		"one byte encoding": {
-			a: Amount{Whole: 123, Fractional: 66},
-			b: Amount{Whole: 123, Fractional: 270},
+			a:            Amount{Whole: 123, Fractional: 66},
+			b:            Amount{Whole: 123, Fractional: 270},
 			expectBigger: false,
 		},
 		"multi byte encoding": {
-			a: Amount{Whole: 260, Fractional: 66},
-			b: Amount{Whole: 123, Fractional: 270},
+			a:            Amount{Whole: 260, Fractional: 66},
+			b:            Amount{Whole: 123, Fractional: 270},
 			expectBigger: true,
 		},
 	}
@@ -67,4 +67,46 @@ func TestAmountLexographic(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAmountGreater(t *testing.T) {
+	cases := map[string]struct {
+		a        Amount
+		b        Amount
+		expected bool
+	}{
+		"fractional diff, lower": {
+			a:        Amount{Whole: 123, Fractional: 66},
+			b:        Amount{Whole: 123, Fractional: 270},
+			expected: false,
+		},
+		"fractional diff, higher": {
+			a:        Amount{Whole: 123, Fractional: 270},
+			b:        Amount{Whole: 123, Fractional: 88},
+			expected: true,
+		},
+		"fractional diff, equal": {
+			a:        Amount{Whole: 123, Fractional: 267},
+			b:        Amount{Whole: 123, Fractional: 267},
+			expected: false,
+		},
+		"whole diff, greater": {
+			a:        Amount{Whole: 187, Fractional: 267},
+			b:        Amount{Whole: 123, Fractional: 267},
+			expected: true,
+		},
+		"whole diff, lower, fractional greater": {
+			a:        Amount{Whole: 187, Fractional: 267},
+			b:        Amount{Whole: 188, Fractional: 0},
+			expected: false,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			greater := tc.a.Greater(&tc.b)
+			assert.Equal(t, tc.expected, greater)
+		})
+	}
+
 }
