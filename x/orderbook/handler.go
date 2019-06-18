c2c2d93f6f3eb646e2240ce117033131dba83a47
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-18 13:16:15 +0200

    Begin CreateOrderHandler

--- x/orderbook/handler.go
+++ x/orderbook/handler.go
@@ -10,6 +10,8 @@ import (
 const (
 	packageName            = "orderbook"
 	newOrderBookCost int64 = 100
+	newOrderCost     int64 = 10
+	cancelOrderCost  int64 = 10
 )
 
 // RegisterQuery registers exchange buckets for querying.
@@ -104,4 +106,118 @@ func (h OrderBookHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weave.
 
 	// we return the new id on creation to enable easier queries
 	return &weave.DeliverResult{Data: orderbook.ID}, err
+}
+
+// CreateOrderHandler will handle creating orderbooks
+type CreateOrderHandler struct {
+	auth            x.Authenticator
+	orderBucket     *OrderBucket
+	tradeBucket     *TradeBucket
+	orderBookBucket *OrderBookBucket
+	marketBucket    *MarketBucket
+}
+
+var _ weave.Handler = CreateOrderHandler{}
+
+// NewCreateOrderHandler creates a handler that allows issuer to
+// create orderbooks. Only owner/admin of the market can issue
+// new orderbooks
+func NewCreateOrderHandler(auth x.Authenticator) weave.Handler {
+	return CreateOrderHandler{
+		auth:            auth,
+		orderBucket:     NewOrderBucket(),
+		tradeBucket:     NewTradeBucket(),
+		orderBookBucket: NewOrderBookBucket(),
+		marketBucket:    NewMarketBucket(),
+	}
+}
+
+// Check just verifies it is properly formed and returns
+// the cost of executing it.
+func (h CreateOrderHandler) Check(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*weave.CheckResult, error) {
+	_, _, err := h.validate(ctx, db, tx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &weave.CheckResult{GasAllocated: newOrderCost}, nil
+}
+
+// validate does all common pre-processing between Check and Deliver
+func (h CreateOrderHandler) validate(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*CreateOrderMsg, *OrderBook, error) {
+	var msg CreateOrderMsg
+
+	if err := weave.LoadMsg(tx, &msg); err != nil {
+		return nil, nil, errors.Wrap(err, "load msg")
+	}
+
+	// Get rules from the orderbook
+	var orderBook OrderBook
+	if err := h.orderBookBucket.One(db, msg.OrderBookID, &orderBook); err != nil {
+		return nil, nil, errors.Wrap(err, "load orderbook")
+	}
+
+	// make sure we have a valid ticker
+	t := msg.Offer.Ticker
+	if t != orderBook.AskTicker && t != orderBook.BidTicker {
+		return nil, nil, errors.Wrap(errors.ErrCurrency, "offer ticker not in this orderbook")
+	}
+
+	// ensure they can make this trade
+	if msg.Trader != nil && !h.auth.HasAddress(ctx, msg.Trader) {
+		return nil, nil, errors.Wrap(errors.ErrUnauthorized, "Must be authorized by the Trader")
+	}
+
+	// TODO: do we want to check the market it belongs to, to check fee payments, etc?
+
+	return &msg, &orderBook, nil
+}
+
+// Deliver creates an orderbook and saves if all preconditions are met
+func (h CreateOrderHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*weave.DeliverResult, error) {
+	msg, ob, err := h.validate(ctx, db, tx)
+	if err != nil {
+		return nil, err
+	}
+
+	now, err := weave.BlockTime(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "No block time in header")
+	}
+
+	trader := msg.Trader
+	if trader == nil {
+		trader = x.MainSigner(ctx, h.auth).Address()
+	}
+
+	side := Side_Ask
+	if msg.Offer.Ticker == ob.BidTicker {
+		side = Side_Bid
+	}
+
+	order := &Order{
+		Metadata:       &weave.Metadata{},
+		OrderBookID:    msg.OrderBookID,
+		Trader:         trader,
+		Side:           side,
+		OrderState:     OrderState_Open,
+		OriginalOffer:  msg.Offer,
+		RemainingOffer: msg.Offer,
+		Price:          msg.Price,
+		CreatedAt:      weave.AsUnixTime(now),
+		UpdatedAt:      weave.AsUnixTime(now),
+		TradeIds:       nil,
+	}
+
+	// Save Order
+	if err := h.orderBucket.Put(db, order); err != nil {
+		return nil, err
+	}
+
+	// TODO: Send money to Address
+
+	// TODO: run through match making on this, re-save each match
+
+	// we return the new id on creation to enable easier queries
+	return &weave.DeliverResult{Data: order.ID}, err
 }
