35d4fe94e4bc955ab158bddfddddf8b92ec5aac2
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-18 13:40:06 +0200

    Add CancelOrderHandler

--- x/orderbook/handler.go
+++ x/orderbook/handler.go
@@ -2,6 +2,7 @@ package orderbook
 
 import (
 	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/coin"
 	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/migration"
 	"github.com/iov-one/weave/x"
@@ -168,7 +169,7 @@ func (h CreateOrderHandler) validate(ctx weave.Context, db weave.KVStore, tx wea
 
 	// ensure they can make this trade
 	if msg.Trader != nil && !h.auth.HasAddress(ctx, msg.Trader) {
-		return nil, nil, errors.Wrap(errors.ErrUnauthorized, "Must be authorized by the Trader")
+		return nil, nil, errors.Wrap(errors.ErrUnauthorized, "must be authorized by the Trader")
 	}
 
 	// TODO: do we want to check the market it belongs to, to check fee payments, etc?
@@ -185,7 +186,7 @@ func (h CreateOrderHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weav
 
 	now, err := weave.BlockTime(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "No block time in header")
+		return nil, errors.Wrap(err, "no block time in header")
 	}
 
 	trader := msg.Trader
@@ -229,4 +230,92 @@ func (h CreateOrderHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weav
 
 	// we return the new id on creation to enable easier queries
 	return &weave.DeliverResult{Data: order.ID}, err
+}
+
+// CancelOrderHandler will handle creating orderbooks
+type CancelOrderHandler struct {
+	auth        x.Authenticator
+	mover       cash.CoinMover
+	orderBucket *OrderBucket
+}
+
+var _ weave.Handler = CreateOrderHandler{}
+
+// NewCancelOrderHandler creates a handler that allows issuer to
+// create orderbooks. Only owner/admin of the market can issue
+// new orderbooks
+func NewCancelOrderHandler(auth x.Authenticator, mover cash.CoinMover) weave.Handler {
+	return CancelOrderHandler{
+		auth:        auth,
+		mover:       mover,
+		orderBucket: NewOrderBucket(),
+	}
+}
+
+// Check just verifies it is properly formed and returns
+// the cost of executing it.
+func (h CancelOrderHandler) Check(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*weave.CheckResult, error) {
+	_, err := h.validate(ctx, db, tx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &weave.CheckResult{GasAllocated: newOrderCost}, nil
+}
+
+// validate does all common pre-processing between Check and Deliver
+func (h CancelOrderHandler) validate(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*Order, error) {
+	var msg CancelOrderMsg
+
+	if err := weave.LoadMsg(tx, &msg); err != nil {
+		return nil, errors.Wrap(err, "load msg")
+	}
+
+	// Load existing order
+	var order Order
+	if err := h.orderBucket.One(db, msg.OrderID, &order); err != nil {
+		return nil, errors.Wrap(err, "load order")
+	}
+
+	// ensure the order is still open
+	if order.OrderState != OrderState_Open {
+		return nil, errors.Wrap(errors.ErrState, "can only cancel open orders")
+	}
+
+	// ensure the trader authorized it
+	if !h.auth.HasAddress(ctx, order.Trader) {
+		return nil, errors.Wrap(errors.ErrUnauthorized, "must be authorized by the Trader")
+	}
+
+	// TODO: do we want to check the market it belongs to, to check fee payments, etc?
+	return &order, nil
+}
+
+// Deliver creates an orderbook and saves if all preconditions are met
+func (h CancelOrderHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*weave.DeliverResult, error) {
+	order, err := h.validate(ctx, db, tx)
+	if err != nil {
+		return nil, err
+	}
+
+	now, err := weave.BlockTime(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "no block time in header")
+	}
+
+	// Return funds
+	err = h.mover.MoveCoins(db, OrderAddress(order), order.Trader, *order.RemainingOffer)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot cover order")
+	}
+
+	// Mark the order as closed
+	order.OrderState = OrderState_Cancel
+	order.RemainingOffer = &coin.Coin{Ticker: order.RemainingOffer.Ticker}
+	order.UpdatedAt = weave.AsUnixTime(now)
+	if err := h.orderBucket.Put(db, order); err != nil {
+		return nil, err
+	}
+
+	return &weave.DeliverResult{}, err
 }
