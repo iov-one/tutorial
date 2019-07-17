2ae3c4463e07429bdc3858badc811312e82ef07d
orkunkl <kulceorkun@gmail.com>
2019-07-17 16:23:25 +0300

    Fix application name

--- app/init.go
+++ app/init.go
@@ -94,7 +94,7 @@ func GenerateApp(options *server.Options) (abci.Application, error) {
 	}
 
 	stack := Stack(options.MinFee)
-	application, err := Application("mycoin", stack, TxDecoder, dbPath, options.Debug)
+	application, err := Application("dex", stack, TxDecoder, dbPath, options.Debug)
 	if err != nil {
 		return nil, err
 	}
