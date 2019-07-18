f799c3017843e7013600c2f2b083d598db48dc1a
orkunkl <kulceorkun@gmail.com>
2019-07-18 17:37:14 +0300

    Add chain function

--- app/app.go
+++ app/app.go
@@ -1,13 +1,33 @@
 package app
 
-import (	
+import (
+	"github.com/iov-one/weave/app"
+	"github.com/iov-one/weave/coin"
 	"github.com/iov-one/weave/x"
-	"github.com/iov-one/weave/x/sigs"
 	"github.com/iov-one/weave/x/multisig"
+	"github.com/iov-one/weave/x/sigs"
+	"github.com/iov-one/weave/x/utils"
+	"github.com/iov-one/weave/x/msgfee"
 )
 
-// Authenticator returns authentication with multisigs 
-// and public key signatues 
+// Authenticator returns authentication with multisigs
+// and public key signatues
 func Authenticator() x.Authenticator {
 	return x.ChainAuth(sigs.Authenticate{}, multisig.Authenticate{})
+}
+
+// Chain returns a chain of decorators, to handle authentication,
+// fees, logging, and recovery
+func Chain(authFn x.Authenticator, minFee coin.Coin) app.Decorators {
+	
+	// TODO implement orderbook controller
+	return app.ChainDecorators(
+		utils.NewLogging(),
+		utils.NewRecovery(),
+		utils.NewKeyTagger(),
+		utils.NewSavepoint().OnCheck(),
+		sigs.NewDecorator(),
+		multisig.NewDecorator(authFn),
+		msgfee.NewFeeDecorator(),
+	)
 }
